7f3a046db2d7cd8db5102fcfc01ce039839c58f7
“knight” <1397843201@qq.com>
2024-10-08 18:25:08 +0800

    Site updated: 2024-10-08 18:25:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/08/Online Engineering Learning 1/","2024/10/08/Overview of online learning/","2024/10/08/hello-world/"];function toRandomPost(){
+var posts=["2024/10/08/hello-world/","2024/10/08/Online Engineering Learning 1/","2024/10/08/Overview of online learning/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
